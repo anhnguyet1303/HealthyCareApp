b1e577cb4493aad7c4b9984366dea2333b2d603f
anhnguyet <anhnguyet1303ttg@gmail.com>
2025-03-06 00:19:40 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/fpt/healthcareproject/LoginActivity.java
@@ -0,0 +1,51 @@
+package vn.edu.fpt.healthcareproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+ EditText edUserName, edPassword;
+ Button btn;
+ TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edUserName = findViewById(R.id.editTextLoginUserName);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUserName.getText().toString();
+                String password = edPassword.getText().toString();
+                if(username.length()== 0 || password.length() == 0){
+                    Toast.makeText(getApplicationContext(), "Please fill username and password", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+
+            }
+        });
+    }
+}
